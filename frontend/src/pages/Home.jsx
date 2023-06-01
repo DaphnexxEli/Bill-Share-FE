8fb5e1300a149e7bd864778e1f1771a42d43405a
DaphnexxEliza <wanatchaya.idea@gmail.com>
2023-06-01 21:44:41 +0700

    updated UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <><div className='text-center text-2xl font-bold'>Authentication System<div>Vite React + Django</div></div></>
+    <><div className='text-center text-2xl font-bold'> <div>Let's figure out a fair way to divide the costs for the expenses</div></div></>
   )
 }
