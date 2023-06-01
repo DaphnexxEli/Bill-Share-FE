f8f8b4fef3c1a8a00cea3fd05409c7f31d9822db
DaphnexxEliza <wanatchaya.idea@gmail.com>
2023-06-01 22:07:18 +0700

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import CardContainer from '../components/optionSelection'
 
 export const Home = () => {
   return (
-    <><div className='text-center text-2xl font-bold'> <div>Let's figure out a fair way to divide the costs for the expenses</div></div></>
+    <><div className='text-center text-2xl font-bold'> <div>Let's figure out a fair way to divide the costs for the expenses</div> <CardContainer></CardContainer></div></>
   )
 }
