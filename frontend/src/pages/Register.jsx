f76283a79cf3c048e5dd1e5e51992bf2954e0efb
DaphnexxEliza <wanatchaya.idea@gmail.com>
2023-07-05 22:55:06 +0700

    create add menu page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -41,11 +41,7 @@ export const Register = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Join us now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+    
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
