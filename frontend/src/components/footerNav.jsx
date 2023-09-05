2907a1d1aef4ecd1e5adcd179f3808da0c5822e4
Jedsada <Jedsada_kampen@cmu.ac.th>
2023-09-06 00:18:20 +0700

    change bottom nav

--- frontend/src/components/footerNav.jsx
+++ frontend/src/components/footerNav.jsx
@@ -6,7 +6,10 @@ function Footer() {
 
   return (
     <div className="btm-nav sticky">
-      <button className="hover:active hover:bg-neutral-focus" onClick={() => navigate("/")}>
+      <button
+        className="hover:active hover:bg-neutral-focus"
+        onClick={() => navigate("/")}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5"
@@ -22,23 +25,10 @@ function Footer() {
           />
         </svg>
       </button>
-      <button className="hover:active hover:bg-neutral-focus" onClick={() => navigate("/static")}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-          />
-        </svg>
-      </button>
-      <button className="hover:active hover:bg-neutral-focus" onClick={() => navigate("/history")}>
+      <button
+        className="hover:active hover:bg-neutral-focus"
+        onClick={() => navigate("/history")}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -51,6 +41,25 @@ function Footer() {
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </button>
+      <button
+        className="hover:active hover:bg-neutral-focus"
+        onClick={() => navigate("/static")}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
           />
         </svg>
       </button>
